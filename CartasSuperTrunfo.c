60ab9a2c9c024abb35e49d683e77b735c502eaf8
CarlosCodes-555 <carlossouzaa2303@gmail.com>
2025-08-12 23:51:09 +0000

    completei mais o codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,21 +16,39 @@ int main() {
     float area;
     float PIB;
     int numero_de_pontos_turisticos;
-    char estado [20]
+    char estado [20];
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população;
-    ;printf("digite o codigo da carta: \n");
-    scanf("$s", codigo);
+    
+    printf("digite o codigo: \n");
+    scanf("%s", codigo);
 
-    printf("digite o nome da cidade: \n");
+    printf("digite o nome: \n");
+    scanf("%s", nome_da_cidade);
 
+    printf("digite a populacao: \n");
+    scanf("%d", populaçao);
 
+    printf("digite a area: \n");
+    scanf("%f", area);
+
+    printf("digite o PIB: \n");
+    scanf("%f", PIB);
+
+    printf("digite o numero de pontos turisticos: \n");
+    scanf("%d", numero_de_pontos_turisticos);
+
+    printf("digite o estado: \n");
+    scanf("%s", estado);
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    
+
 
     return 0;
 }
